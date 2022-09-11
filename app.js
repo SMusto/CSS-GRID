6f8eac21f284e9b447ccc267a28a88bfe4ae818a
santi <santimworkk@gmail.com>
2022-09-11 15:49:54 -0300

    terminamos navbar

---
+++ app.js
@@ -0,0 +1,6 @@
+let menu_bar = document.querySelector('#menu-bar');
+let menu = document.querySelector('#menu')
+
+menu_bar.addEventListener('click', function(){
+    menu.classList.toggle('menu-toggle')
+})
